1e3ad71af2ad2f2b007b1f8a693c9f4c0bec7cff
Dennis Vinterfjärd <dennis.hedlund@klarna.com>
2023-02-16 17:24:20 +0100

    Added telegram bot sendMessage function

--- src/repository/telegram.ts
+++ src/repository/telegram.ts
@@ -1,6 +1,11 @@
+import TelegramBot from 'node-telegram-bot-api';
 export class Telegram {
+    private chatId: string;
+    private apiKey: string;
+    private debug: boolean;
+    private bot: TelegramBot;
 
-    constructor(private apiKey: string, private chatId: string) {
+    constructor(apiKey: string, chatId: string, debug: boolean = false) {
         if (apiKey === undefined) {
             throw new Error('TELEGRAM_API_KEY is undefined');
         }
@@ -9,5 +14,20 @@ export class Telegram {
         }
         this.apiKey = apiKey;
         this.chatId = chatId;
+        this.debug = debug;
+        this.bot = new TelegramBot(this.apiKey)
+    }
+
+    public async sendMessage(message: string): Promise<void> {
+        await this.bot.sendMessage(this.chatId, message)
+        .catch((error) => {
+            this.bot.sendMessage(this.chatId, 'TELEGRAM_BOT_ERROR: ' + error);
+            console.error(error);
+        })
+        .then((res) => {
+            if (!this.debug) return;
+            console.log('TELEGRAM_BOT_RESPONSE: ' + res);
+            this.bot.sendMessage(this.chatId, 'DEBUG_TELEGRAM_BOT_RESPONSE: ' + res);
+        })
     }
 }
