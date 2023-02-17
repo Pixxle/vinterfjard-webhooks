4393338b3c56b5e62dde740474a9fdada9c98e63
Dennis vinterfjärd <dennis@vinterfjard.com>
2023-02-17 15:36:43 +0100

    replace nodejs telegram with telgraf

--- src/repository/telegram.ts
+++ src/repository/telegram.ts
@@ -1,9 +1,10 @@
-import * as TelegramBot from 'node-telegram-bot-api';
+import { Telegraf } from 'telegraf';
+
 export class Telegram {
     private chatId: string;
     private apiKey: string;
     private debug: boolean;
-    private bot: TelegramBot;
+    private bot: Telegraf;
 
     constructor(apiKey: string, chatId: string, debug: boolean = false) {
         if (apiKey === undefined) {
@@ -15,19 +16,19 @@ export class Telegram {
         this.apiKey = apiKey;
         this.chatId = chatId;
         this.debug = debug;
-        this.bot = new TelegramBot(this.apiKey, { polling: false });
+        this.bot = new Telegraf(this.apiKey);
     }
 
     public async send_message(message: string): Promise<void> {
-        await this.bot.sendMessage(this.chatId, message)
+        this.bot.telegram.sendMessage(this.chatId, message)
         .catch((error) => {
-            this.bot.sendMessage(this.chatId, 'TELEGRAM_BOT_ERROR: ' + error);
+            this.bot.telegram.sendMessage(this.chatId, 'TELEGRAM_BOT_ERROR: ' + error);
             console.error(error);
         })
         .then((res) => {
             if (!this.debug) return;
             console.log(`TELEGRAM_BOT_RESPONSE: ${res}`);
-            this.bot.sendMessage(this.chatId, `DEBUG_TELEGRAM_BOT_RESPONSE: ${res}`);
+            this.bot.telegram.sendMessage(this.chatId, `DEBUG_TELEGRAM_BOT_RESPONSE: ${res}`);
         })
     }
 }
