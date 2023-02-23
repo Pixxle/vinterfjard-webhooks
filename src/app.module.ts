53365e8f43c4c42f572f9acbdde63530f112005a
Dennis vinterfjärd <dennis@vinterfjard.com>
2023-02-24 00:17:02 +0100

    Refactor Riksarkivet API calls
    - Remove the RiksarkivetController, RiksarkivetModule, RiksarkivetService and associated test files
    - Add NotionService to the providers
    
    [src/riksarkivet/riksarkivet.controller.spec.ts]
    - Remove the RiksarkivetController test file
    [src/app.module.ts]
    - Remove the `RiksarkivetModule` from the imports
    - Add the `NotionService` to the providers
    [src/riksarkivet/riksarkivet.module.ts]
    - Delete the `RiksarkivetModule` file
    [src/riksarkivet/riksarkivet.service.ts]
    - Remove `RiksarkivetService` class
    - Remove `register_hash` function
    - Remove `get_times` function
    - Remove `notify_if_bookable` function
    [src/riksarkivet/riksarkivet.service.spec.ts]
    - Remove the RiksarkivetService test file
    [src/riksarkivet/riksarkivet.controller.ts]
    - Removed the `RiksarkivetController` file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { RiksarkivetModule } from './riksarkivet/riksarkivet.module';
 import { TelegramModule } from './telegram/telegram.module';
 import { ChatgptService } from './chatgpt/chatgpt.service';
 import { ChatgptModule } from './chatgpt/chatgpt.module';
@@ -7,7 +6,7 @@ import { NotionService } from './notion/notion.service';
 import { NotionModule } from './notion/notion.module';
 
 @Module({
-  imports: [RiksarkivetModule, TelegramModule, ChatgptModule, NotionModule],
+  imports: [TelegramModule, ChatgptModule, NotionModule],
   providers: [ChatgptService, NotionService],
 })
 export class AppModule { }
